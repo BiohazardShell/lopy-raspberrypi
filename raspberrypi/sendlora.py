11e69e614a020a74d7c209002a6a1f72e7f18c8c
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-09 02:20:03 +0100

    changed serial device

--- raspberrypi/sendlora.py
+++ raspberrypi/sendlora.py
@@ -3,7 +3,7 @@ import serial
 
 data = "new entery 01".encode()
 
-with serial.Serial('/dev/serial1', 115200, timeout=10) as ser: # remember to change the timeout from 10 sec to lower if you wnt it to speed up
+with serial.Serial('/dev/ttyAMA0', 115200, timeout=10) as ser: # remember to change the timeout from 10 sec to lower if you wnt it to speed up
     ser.write(data)
 
 print("Data Sent")
