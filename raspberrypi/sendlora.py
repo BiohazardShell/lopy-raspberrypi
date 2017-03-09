b72fff9427aef1d4c8337d8178fa45e7f77547db
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-09 14:29:44 +0100

    Update sendlora.py

--- raspberrypi/sendlora.py
+++ raspberrypi/sendlora.py
@@ -1,7 +1,9 @@
 # run script as super user (e.g. $ sudo python3 sendlora.py)
 import serial
 
+data = b"test data"
+
 with serial.Serial('/dev/serial0', 115200, timeout=10) as ser:
-    ser.write(b'send')
+    ser.write(data)
 
 print("Data Sent")
