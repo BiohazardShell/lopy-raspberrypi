3ba282905d1f76248ebdae3d7633a9cd62776c6b
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-09 14:24:01 +0100

    Update main.py

--- lopy/main.py
+++ lopy/main.py
@@ -9,13 +9,13 @@ import struct
 pycom.heartbeat(False) # turn off heartbeat
 
 uart1 = UART(1, 115200, bits=8, parity=None, stop=1)
-uart1.init(baudrate=115200, bits=8, parity=None, stop=1, timeout_chars=2, pins=("P3", "P4"))
+uart1.init(baudrate=115200, bits=8, parity=None, stop=1, timeout_chars=2, pins=("P3", "P4")) # G11(TX) and G24(RX)
 uart1.write("Connected...")
 
 # Initialize LoRa in LORAWAN mode.
 lora = LoRa(mode=LoRa.LORAWAN)
-app_eui = binascii.unhexlify('AD A4 DA E3 AC 12 67 6B'.replace(' ',''))
-app_key = binascii.unhexlify('11 B0 28 2A 18 9B 75 B0 B4 D2 D8 C7 FA 38 54 8B'.replace(' ',''))
+app_eui = binascii.unhexlify('7079636f6d303031')
+app_key = binascii.unhexlify('6c6f7079636f6d72756c657332303137')
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 
 while not lora.has_joined():
@@ -31,16 +31,15 @@ s.setblocking(True)
 
 def send_lora(data):
     print("Sending " + data)
-    s.send(str(data))
+    s.send(data)
     uart1.write(b'Success')
 
 while True:
     if uart1.any():
         data = uart1.readall()
         pycom.rgbled(0xFF0000) # set LED to RED on if data received
-        if data == b'send':
-            send_lora("data")
-            pycom.rgbled(0x00FF00) # set LED to GREEN if data is b'send'
+        send_lora(str(data))
+        pycom.rgbled(0x00FF00) # set LED to GREEN if data is b'send'
         time.sleep(1)
         pycom.rgbled(0x000000)
     time.sleep(0.25)
