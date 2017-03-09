13ad35db09a203193076aa57384fea8f10f2bff9
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-09 02:07:00 +0100

    correcting serial interface

--- raspberrypi/sendlora.py
+++ raspberrypi/sendlora.py
@@ -1,10 +1,9 @@
 # run script as super user (e.g. $ sudo python3 sendlora.py)
 import serial
 
-data = "new entery 01"
-bytes_data = data.encode('utf-8')
+data = "new entery 01".encode()
 
-with serial.Serial('/dev/serial0', 115200, timeout=10) as ser:
+with serial.Serial('/dev/serial1', 115200, timeout=10) as ser:
     ser.write(bytes_data) # need to find a way to send variables like the data ubove
 
 print("Data Sent")
