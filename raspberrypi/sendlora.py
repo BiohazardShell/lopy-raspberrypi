58f1b981c6ff17067f4dabdf3e4d0f567234735c
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-09 02:08:45 +0100

    correcting rpi sending var

--- raspberrypi/sendlora.py
+++ raspberrypi/sendlora.py
@@ -4,6 +4,6 @@ import serial
 data = "new entery 01".encode()
 
 with serial.Serial('/dev/serial1', 115200, timeout=10) as ser:
-    ser.write(bytes_data) # need to find a way to send variables like the data ubove
+    ser.write(data)
 
 print("Data Sent")
