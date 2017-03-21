7c295d9ed5d165977d96cd0030822f07c65cf720
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-21 17:13:50 +0100

    updating after successful test

--- raspberrypi/sendlora.py
+++ raspberrypi/sendlora.py
@@ -1,9 +1,9 @@
 # run script as super user (e.g. $ sudo python3 sendlora.py)
-import serial
+import serial # because you know ... you need to talk
 
-data = b"test data"
+data = "data to send trought the lora".encode() # ... well ... it's obvious ... I think ...
 
-with serial.Serial('/dev/serial0', 115200, timeout=10) as ser:
-    ser.write(data)
+with serial.Serial('/dev/ttyAMA0', 115200, timeout=10) as ser: # remember to change the timeout from 10 sec to lower if you want it to speed up
+    ser.write(data) # data out trought the Tx . good job !
 
-print("Data Sent")
+print("Data Sent") # In case you want to know ...
